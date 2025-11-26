97aa6856d3dc3fb5c98f41e49ceae196ad2d4d66
DanRCM <rcortezmanzano24@gmail.com>
2025-11-26 14:01:11 -0500

    Componentes agregados

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,42 @@
 import { Grid } from '@mui/material';
+import HeaderUI from './components/HeaderUI';
 import './App.css'
+import AlertUI from './components/AlertUI';
 
 function App() {
-    return (
-      <Grid container spacing={5} justifyContent="center" alignItems="center">
+  return (
+    <Grid container spacing={5} justifyContent="center" alignItems="center">
 
-         {/* Encabezado */}
-         <Grid size={{ xs: 12, md: 12 }}>Elemento: Encabezado</Grid>
+      {/* Encabezado */}
+      <Grid size={{ xs: 12, md: 12 }}>
+        <HeaderUI />
+      </Grid>
 
-         {/* Alertas */}
-         <Grid size={{ xs: 12, md: 12 }}>Elemento: Alertas</Grid>
+      {/* Alertas */}
+      <Grid size={{ xs: 12, md: 12 }} container justifyContent="right" alignItems="center">
 
-         {/* Selector */}
-         <Grid size={{ xs: 12, md: 3 }}>Elemento: Selector</Grid>
-
-         {/* Indicadores */}
-         <Grid size={{ xs: 12, md: 9 }}>Elemento: Indicadores</Grid>
-
-         {/* Gráfico */}
-         <Grid size={{ xs: 12, md: 6 }} sx={{ display: { xs: "none", md: "block"} }}>Elemento: Gráfico</Grid>
-
-         {/* Tabla */}
-         <Grid size={{ xs: 12, md: 6 }} sx={{ display: { xs: "none", md: "block"} }}>Elemento: Tabla</Grid>
-
-         {/* Información adicional */}
-         <Grid size={{ xs: 12, md: 12 }}>Elemento: Información adicional</Grid>
+        <AlertUI description="No se preveen lluvias" />
 
       </Grid>
-        
-    );
+
+      {/* Selector */}
+      <Grid size={{ xs: 12, md: 3 }}>Elemento: Selector</Grid>
+
+      {/* Indicadores */}
+      <Grid size={{ xs: 12, md: 9 }}>Elemento: Indicadores</Grid>
+
+      {/* Gráfico */}
+      <Grid size={{ xs: 12, md: 6 }} sx={{ display: { xs: "none", md: "block" } }}>Elemento: Gráfico</Grid>
+
+      {/* Tabla */}
+      <Grid size={{ xs: 12, md: 6 }} sx={{ display: { xs: "none", md: "block" } }}>Elemento: Tabla</Grid>
+
+      {/* Información adicional */}
+      <Grid size={{ xs: 12, md: 12 }}>Elemento: Información adicional</Grid>
+
+    </Grid>
+
+  );
 }
 
 export default App;
